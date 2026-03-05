845f5a3b735b39b166db846237da097b74d96964
brayanDRONE <brayan_96321@hotmail.com>
2026-03-05 18:35:08 -0300

    Agregar Vercel Analytics para monitorear accesos a la aplicación

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { Analytics } from '@vercel/analytics/react'
 import './theme.css'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Analytics />
     <App />
   </React.StrictMode>,
 )
